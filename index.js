3cc3c5cf6eb57eb9a93ca819148516b0b16baf2c
Wamuyu <michellewachira25@gmail.com>
2022-06-04 23:43:05 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const employee ={ 
-     name: "Alex",
-    streetAddress:"Nairobi",
-};
+let employee ={
+    name: "Sam",
+    streetAddress:"11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const newObj={...employee};
+       newObj[key]=value;
+        return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value)
+    {
+        employee[key] = value;
+        return employee;
+  }
+   
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj ={...employee}
+   delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}  
